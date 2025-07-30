ce83843f7336c756096fb41a76e1b436c272c1ec
Rafael Ribeiro <rafaribe746@gmail.com>
2025-07-29 22:41:20 -0300

    começando tela login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,18 @@
+import { Flex, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import loginImage from "../../public/assets/login-image.gif";
+
 export default function Login() {
-    return( 
-    <div>
-        Login
-    </div>)
+    return(
+    <Flex w="100vw" h="100vh">
+        <Flex w="50%" bg="#2C73EB" align="center" justify="center">
+
+          <Image w={500} h={500} src={loginImage.src}/>
+
+
+        </Flex>
+        <HStack w="50%" bg="green.400">
+          <Heading as="h1">Account Login</Heading>
+          <Text>If you are already a member you can login with your email address and password.</Text>
+        </HStack>
+    </Flex>)
 }
