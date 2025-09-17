c884b9d61f39270c6b624733f2451a95c60b5720
Rafael Ribeiro <rafaribe746@gmail.com>
2025-09-16 22:38:27 -0300

    desenvolvendo students

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
-import { Card, ColorPalette, HStack, Stack, VStack } from "@chakra-ui/react";
+import {
+  Card,
+  ColorPalette,
+  HStack,
+  SimpleGrid,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { AiFillQuestionCircle } from "react-icons/ai";
 import { BiAward } from "react-icons/bi";
 import { FaUserGraduate } from "react-icons/fa";
 import { FaArrowTrendUp } from "react-icons/fa6";
-import { IoMdCalendar, IoMdPeople } from "react-icons/io";
-import { MdLibraryBooks } from "react-icons/md";
-import { IndicatorCard } from "@/components/IndicatorCard";
-import { CustomCard } from "@/components/CustomCard";
-import { DefaultLayout } from "@/layouts/DefaultLayout";
 import { ImCalendar } from "react-icons/im";
-import { IconType } from "react-icons";
-import { AiFillQuestionCircle } from "react-icons/ai";
+import { IoMdCalendar, IoMdPeople } from "react-icons/io";
 import { IoAlertCircle } from "react-icons/io5";
+import { MdLibraryBooks } from "react-icons/md";
+import { CustomCard } from "@/components/CustomCard";
+import { IndicatorCard } from "@/components/IndicatorCard";
+import { DefaultLayout } from "@/layouts/DefaultLayout";
 
 export default function Home() {
   const recentActivities = [
@@ -45,27 +52,26 @@ export default function Home() {
       id: 1,
       type: "exam",
       title: "Inicio das provas finais",
-      color:"red",
-      time: "14 Dez 2025 08:00",
+      color: "red",
+      time: "14 Dez 2025 ás 08:00",
     },
     {
       id: 2,
       type: "meeting",
       title: "Reunião de coordenadores",
-      color:"blue",
+      color: "blue",
       time: "18 Dez 2025 ás 14:00",
     },
     {
       id: 3,
       type: "deadline",
       title: "Fim do Semestre",
-      color:"orange",
+      color: "orange",
       time: "20 Dez 2025 ás 17:00",
     },
-
   ];
 
-  const iconMap: Record<string, IconType> = {
+  const iconMapActivities: Record<string, IconType> = {
     enrollment: IoMdPeople,
     grade: BiAward,
     course: MdLibraryBooks,
@@ -76,94 +82,94 @@ export default function Home() {
     exam: IoAlertCircle,
     meeting: IoMdPeople,
     deadline: IoMdCalendar,
-
   };
   return (
     <DefaultLayout
       title="Dashboard acadêmica"
       description="visão geral do sistema universitário"
     >
-      <HStack >
-        <IndicatorCard
-          icon={IoMdPeople}
-          label={"Total de estudantes"}
-          value={2847}
-          indicator="-12% este mês"
-          colorPallete="blue"
-        />
+      <SimpleGrid row={2}>
+        <HStack gap={8}>
+          <IndicatorCard
+            icon={IoMdPeople}
+            label={"Total de estudantes"}
+            value={2847}
+            indicator="-12% este mês"
+            colorPallete="blue"
+          />
 
-        <IndicatorCard
-          icon={FaUserGraduate}
-          label={"Total de professores"}
-          value={25}
-          indicator="+15% este mês"
-          colorPallete="red"
-        />
+          <IndicatorCard
+            icon={FaUserGraduate}
+            label={"Total de professores"}
+            value={25}
+            indicator="+15% este mês"
+            colorPallete="red"
+          />
 
-        <IndicatorCard
-          icon={MdLibraryBooks}
-          label={"Cursos oferecidos"}
-          value={34}
-          indicator="+20% este mês"
-          colorPallete="yellow"
-        />
+          <IndicatorCard
+            icon={MdLibraryBooks}
+            label={"Cursos oferecidos"}
+            value={34}
+            indicator="+20% este mês"
+            colorPallete="yellow"
+          />
 
-        <IndicatorCard
-          icon={FaArrowTrendUp}
-          label={"Taxa de aprovação"}
-          value={0.892}
-          style="percent"
-          minimumFractionDigits={2}
-          maximumFractionDigits={2}
-          indicator="+18% este mês"
-          colorPallete="green"
-        />
-      </HStack>
+          <IndicatorCard
+            icon={FaArrowTrendUp}
+            label={"Taxa de aprovação"}
+            value={0.892}
+            style="percent"
+            minimumFractionDigits={2}
+            maximumFractionDigits={2}
+            indicator="+18% este mês"
+            colorPallete="green"
+          />
+        </HStack>
 
-      <HStack mt={8} gap={8} align="start">
-        <Card.Root >
-          <Card.Header>
-            <Card.Title>Atividades recentes</Card.Title>
-          </Card.Header>
-          <Card.Body gap={10}>
-            {recentActivities.map((activity) => {
-              const ActivityIcon =
-                iconMap[activity.type] ?? AiFillQuestionCircle;
-              return (
-                <CustomCard
-                  key={activity.id}
-                  color="blue"
-                  icon={ActivityIcon}
-                  title={activity.title}
-                  dateTime={activity.time}
-                />
-              );
-            })}
-          </Card.Body>
-        </Card.Root>
+        <HStack mt={8} gap={8} align="start">
+          <Card.Root>
+            <Card.Header>
+              <Card.Title>Atividades recentes</Card.Title>
+            </Card.Header>
+            <Card.Body gap={10}>
+              {recentActivities.map((activity) => {
+                const ActivityIcon =
+                  iconMapActivities[activity.type] ?? AiFillQuestionCircle;
+                return (
+                  <CustomCard
+                    key={activity.id}
+                    color="blue"
+                    icon={ActivityIcon}
+                    title={activity.title}
+                    dateTime={activity.time}
+                  />
+                );
+              })}
+            </Card.Body>
+          </Card.Root>
 
-        <Card.Root >
-          <Card.Header>
-            <Card.Title>Próximos eventos</Card.Title>
-          </Card.Header>
-          <Card.Body gap={10}>
-            {customCards.map((events) => {
-              const EventsIcon =
-                iconMapEvents[events.type] ?? AiFillQuestionCircle;
-              return (
-                <CustomCard
-                  key={events.id}
-                  color={events.color as ColorPalette}
-                  icon={EventsIcon}
-                  title={events.title}
-                  dateTime={events.time}
-                />
-              );
-            })}
-          </Card.Body>
-        </Card.Root>
-      </HStack>
-
+          <Card.Root>
+            <Card.Header>
+              <Card.Title>Próximos eventos</Card.Title>
+            </Card.Header>
+            <Card.Body gap={10}>
+              {customCards.map((events) => {
+                const EventsIcon =
+                  iconMapEvents[events.type] ?? AiFillQuestionCircle;
+                return (
+                  <CustomCard
+                    key={events.id}
+                    color={events.color as ColorPalette}
+                    icon={EventsIcon}
+                    title={events.title}
+                    dateTime={events.time}
+                  />
+                );
+              })}
+            </Card.Body>
+          </Card.Root>
+        </HStack>
+      </SimpleGrid>
     </DefaultLayout>
   );
 }
--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -1,14 +1,38 @@
+import { Card, HStack, Input, InputGroup, SimpleGrid } from "@chakra-ui/react";
+import { LuSearch } from "react-icons/lu";
 import { DefaultLayout } from "@/layouts/DefaultLayout";
-import { Heading } from "@chakra-ui/react";
 
 export default function Students() {
-    return(
-         <DefaultLayout
+  return (
+    <DefaultLayout
+      title="Gestão de Estudantes"
+      description="Gerencie informações dos estudantes matriculados"
+    >
+      <HStack>
+        {/*
+            Campo de pesquisa - SearchInput
 
-          title="estudantes"
-          description="estudadmaldmma fwfefwf f"
-          >
-          <Heading color="blue">estudantes</Heading>
-        </DefaultLayout>
-      )
+
+            Select - Filtro de Status -
+            Button - Filtros -
+            Button - Exportar -
+            Button - Action Button - Criar um novo estudante
+
+            */}
+        <Card.Root>
+          <Card.Body>
+            <SimpleGrid row={2} rowGap={4}>
+            <InputGroup flex="1" startElement={<LuSearch />}>
+              <Input placeholder="Buscar por Nome, Email ou curso..." />
+            </InputGroup>
+
+            <HStack>
+
+            </HStack>
+            </SimpleGrid>
+          </Card.Body>
+        </Card.Root>
+      </HStack>
+    </DefaultLayout>
+  );
 }
